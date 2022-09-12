74029b8f9ad410e0c7489b7a60f1b985d335bca7
Jakub Jamroz <jakubjamroz23@gmail.com>
2022-09-12 19:51:46 +0200

    timer start, timer stop, make timer start after start button, and make ball static until start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 14;
-const cellsVertical = 10;
+const cellsHorizontal = 4;
+const cellsVertical = 3;
 const width = window.innerWidth;
 const height = window.innerHeight * 0.9;
 
@@ -207,6 +207,7 @@ World.add(world, goal);
 const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: "ball",
+  isStatic: true,
   render: {
     fillStyle: "#fcc419",
   },
--- utilities.js
+++ utilities.js
@@ -1,16 +1,48 @@
+// Buttons
 const startBtn = document.querySelector(".start-btn");
 const nextBtn = document.querySelector(".next");
 
+// Inputs
+const timer = document.querySelector(".timer");
+const scoreBoard = document.querySelector(".scoreboard");
+const level = document.querySelector(".level");
+
 // start button
 startBtn.addEventListener("click", (event) => {
+  Body.setStatic(ball, false);
   startBtn.classList.add("hidden");
+  timerStart();
 });
 
-// after win
+// after win message
 const winnerMeassage = () => {
   document.querySelector(".winner").classList.remove("hidden");
   setTimeout(() => {
     document.querySelector(".winner").classList.add("hidden");
     nextBtn.classList.remove("hidden");
   }, 7000);
+};
+
+// next game start
+nextBtn.addEventListener("click", (event) => {
+  location.reload();
+});
+
+let interval;
+// TIMER START
+const timerStart = () => {
+  interval = setInterval(() => {
+    let timeLeft = timer.innerHTML;
+
+    if (timeLeft > 0) {
+      timer.innerHTML = timeLeft - 1;
+    } else {
+      clearInterval(interval);
+    }
+  }, 1000);
+};
+
+// TIMER STOP
+const timerStop = () => {
+  clearInterval(interval);
 };
